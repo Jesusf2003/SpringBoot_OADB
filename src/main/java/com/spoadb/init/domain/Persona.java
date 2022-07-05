9898bb6a2260348bd8bc9a7bd2e0d349ece2f012
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-07-04 23:11:12 -0500

    Iniciando proyecto de Springboot con Oracle Autonomous Data Base.

---
+++ src/main/java/com/spoadb/init/domain/Persona.java
@@ -0,0 +1,13 @@
+package com.spoadb.init.domain;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+
+	private Long person_id;
+	private String first_name;
+	private String last_lname;
+}
